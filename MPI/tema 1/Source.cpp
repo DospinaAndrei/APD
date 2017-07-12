e6a2d02ae3bcf65a24cb4b19c703959bdd546cad
Dospina Andrei <dspandrei@yahoo.com>
2017-07-13 00:35:57 +0300

    Update Source.cpp

--- MPI/tema 1/Source.cpp
+++ MPI/tema 1/Source.cpp
@@ -24,7 +24,7 @@ bool isprime(int n)
 	if ((n % 2)) {
 		return false;  // cand gaseste nr impar returneaza 0-fals
 	}
-	for (int i = 2; i*i <= n; i++) { 
+	for (int i = 2; i <= n/2; i++) { 
 		if (!(n % i)) {   //verifica daca este nr prim
 			return 0;
 		}
