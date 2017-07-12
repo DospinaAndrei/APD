c81bc5a0e54669789f6c6b93d4c573824d23506d
Dospina Andrei <dspandrei@yahoo.com>
2017-07-12 14:35:29 +0300

    Update Source.cpp

--- MPI/tema 1/Source.cpp
+++ MPI/tema 1/Source.cpp
@@ -1,50 +1,118 @@
+//Tema1_numar prim
+
+//Fiecare proces lansat ia o parte de la 0 la 10; de la 11 la 20; de la 21 la 30; de la 31 la 40 
+//si le verifica pe astea daca sunt prime iar daca sunt le printeaza.
+// daca sunt in radacina calculez fiecare ce parte sa ia
+//daca sunt pe un proces 1-2-3 primesc datele si lep relucrez
+
+#include <iostream>
 #include "mpi.h"
-#include <stdio.h>
-#include <cstdlib>
-#include <math.h>
-#define MAXSIZE 100
-#define SNR 10
+#include <string>
+#define FirstRank 0
+#define N 40
 
-int main(int argc, char **argv)
+using namespace std;
+
+bool isprime(int n)
 {
-	int myid, numprocs;
-	int data[MAXSIZE], i, x, low, high, myresult = 0, result;
-	
-
-	MPI_Init(&argc, &argv);
-
-	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-
-
-if (0 == myid) {
-	for (i = 0; i < MAXSIZE; i++) {
-		data[i] = rand() %20 + 1;
-		printf("%d   ", data[i]);
+	if (n <= 1) { // nu este natural
+		return 0;
 	}
-}
-printf("\n");
-/* broadcast data */
-//Broadcasts a message from the process with rank "root" to all other processes of the communicator
-//Use MPI_Broadcast for sending the array
-MPI_Bcast(data, MAXSIZE, MPI_INT, 0, MPI_COMM_WORLD);
-
-/* add portion of data */
-x = MAXSIZE / numprocs; /* must be an integer *///impart in bucati vectorul, fiecare proces face o parte
-low = myid * x;
-high = low + x;
-for (i = low; i<high; i++) {
-	if (data[i] == SNR) {
-		myresult = i;
+	if (n == 2) {
+		return true;
 	}
+	if (!(n % 2)) {
+		return false;  // cand gaseste nr impar returneaza 0-fals
+	}
+	for (int i = 2; i*i <= n; i++) { 
+		if (!(n % i)) {   //verifica daca este nr prim
+			return 0;
+		}
+	}
+	return 1;
 }
-printf("I got %d from %d\n", myresult, myid);
 
-//Combines the values sent by all processes using a predefined operator and places result in the receive buffer of the root process.
-//For computing the maximum position, you need to use MPI_Reduce.
-MPI_Reduce(&myresult, &result, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-if (0 == myid) {
-	printf("The maximum position is %d.\n", result);
-}
-MPI_Finalize();
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	int  my_rank;       /* rank of process (0, 1, 2, 3)*/
+	int  nrProcesses;   /* number of processes (se lanseaza 4 procese) */
+	int  nameSize;      /* length of name */
+
+	int  parte = 0;       /*parte pentru fiecare proces*/
+	int count = 0;     //numerele prime care vor fi afisate
+
+	int SendData = 0;   //ce trimite radacina
+	int ReceiveData = 0;  //ce primeste procesul copil
+
+	char computerName[MPI_MAX_PROCESSOR_NAME];       //DESKTOP-VE4Q72I
+
+	MPI_Init(&argc, &argv); /*START MPI */
+
+							/*Stabilește dimensiunea grupului asociat unui comunicator(cate procese sa imi fie lansate)*/
+	MPI_Comm_size(MPI_COMM_WORLD, &nrProcesses);
+
+	/*Stabilește rangul procesului de apelare în comunicator || Rangul procesului curent*/
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	/*Obține numele procesorului si stocheaza dimensiunea acestuia in buffer*/
+	MPI_Get_processor_name(computerName, &nameSize);
+
+	if (my_rank == FirstRank) { //daca sunt in radacina
+
+		parte = N / nrProcesses;
+
+		cout << "Hello from process " << my_rank << " of " << nrProcesses << " processor on " << computerName << "\n\n";
+
+		for (count = parte*FirstRank;count < parte*(FirstRank + 1);count++)  //incepe de la 0 ca FirstRank=0, apoi creste contorul. Deci se stabileste cine ce numar sa ia.
+		{
+			if (isprime(count)) {
+				cout << count << "  ";
+			}
+		}
+
+		SendData = parte;
+		for (i = 1; i < nrProcesses; i++)
+		{
+			//int MPI_Send(void *buf -Datele care trebuie trimise / primite (tampon aplicație). 
+			//int count -(Dimensiunea (numărul de elemente) a tamponului care trebuie trimis / primit.) 
+			//MPI_Datatype datatype, -Una dintre următoarele valori
+			//int dest -(Rangul proceselor receptorului) , 
+			//int tag -(eticheta mesajului primire/trimitere(+-), 
+			//MPI_Comm comm)
+			//
+			MPI_Send(&SendData, 1, MPI_INT, i, 1, MPI_COMM_WORLD);   //trimite date
+		}
+
+	}
+
+	MPI_Status status;
+	if (my_rank > FirstRank) {   //daca sunt pe alt proces
+		MPI_Recv(&ReceiveData, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);     //primeste date
+
+																			   //int MPI_Send(void *buf -Datele care trebuie trimise / primite (tampon aplicație). 
+																			   //int count -(Dimensiunea (numărul de elemente) a tamponului care trebuie trimis / primit.) 
+																			   //MPI_Datatype datatype, -Una dintre următoarele valori
+																			   //int dest -(Rangul proceselor receptorului) , 
+																			   //int tag -(eticheta mesajului primire/trimitere(+-), 
+																			   //MPI_Comm comm)
+																			   // MPI_Status *status  -Structură care indică sursa și eticheta mesajului.
+
+
+
+																			   //cout<<"Receive data " <<ReceiveData;
+		cout << "Hello from process " << my_rank << " of " << nrProcesses << " processor on " << computerName << "\n\n";
+		for (count = ReceiveData*my_rank;count < ReceiveData*(my_rank + 1);count++)
+		{
+			if (isprime(count)) {
+				cout << count << "  ";
+			}
+		}
+	}
+
+	MPI_Finalize(); /* EXIT MPI */
+
+	return 0;
 }
